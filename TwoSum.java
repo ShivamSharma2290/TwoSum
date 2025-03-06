32cb91142943f5f5a7a6bb98c741249cc21202fe
Shivam Sharma <shivsharma4108@gmail.com>
2025-03-06 22:29:17 +0530

    Add files via upload

---
+++ TwoSum.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+class Solution {
+    // two pointers approach
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+    }
+}
+
+public class TwoSum {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        
+        int[] result = solution.twoSum(nums, target);
+        
+        if (result != null) {
+            System.out.println("Indices: " + Arrays.toString(result));
+        } else {
+            System.out.println("No solution found.");
+        }
+    }
+}
